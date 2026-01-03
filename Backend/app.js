8b522726649450cfd0a68614de3a5b0fb5e74fd3
user.name like: "hemanta.rayamajhi2@gmail.com <user.email: "hemanta.rayamajhi2@gmail.com>
2026-01-03 14:42:37 +0545

    registeruser n usercreate

--- Backend/app.js
+++ Backend/app.js
@@ -3,19 +3,25 @@ dotenv.config();
 
 
 const express = require('express');
+const app= express();
 const path = require('path');
 const cors =require('cors');
+const userRoutes = require('./routes/user.routes');
+
+const connectToDb = require('./db/db');
+connectToDb();
 
 
-const app= express();
 
 
 
 app.use(cors());
-
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get('/',(req, res)=>{
 res.send('hi');
 });
+app.use('/users', userRoutes);
 
 module.exports=app;
