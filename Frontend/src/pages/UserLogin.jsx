21a020d24cab3fc182769c29f278bacfaa02c06d
user.name like: "hemanta.rayamajhi2@gmail.com <user.email: "hemanta.rayamajhi2@gmail.com>
2026-01-05 23:39:28 +0545

    home and login page created

---
+++ Frontend/src/pages/UserLogin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserLogin = () => {
+  return (
+    <div className='w-full h-screen pt-8 pl-8'>
+      <h3 className='text-xl text-bold'>What's your Email ? </h3>
+    </div>
+  )
+}
+
+export default UserLogin
